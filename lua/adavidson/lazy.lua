9c7fdb4a345655dd4f2fea1c63d9a8edab9ae5f7
AlecJDavidson <alecjdavidson@outlook.com>
2025-09-28 20:10:11 -0400

    Adding gitsigns back in. Lazygit is still borked.

--- lua/adavidson/lazy.lua
+++ lua/adavidson/lazy.lua
@@ -3,14 +3,14 @@ local fn = vim.fn
 local lazypath = fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not vim.loop.fs_stat(lazypath) then
-  fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -20,221 +20,240 @@ local options = {}
 -- plugins
 local plugins = {
 
-  -- Gen.nvim
-  -- Minimal configuration
-  {
-    "David-Kunz/gen.nvim",
-    config = function()
-      require("adavidson.plugins.treesitter")
-    end,
-  },
+	-- Gen.nvim
+	-- Minimal configuration
+	{
+		"David-Kunz/gen.nvim",
+		config = function()
+			require("adavidson.plugins.treesitter")
+		end,
+	},
 
-  -- Svelte
-  {
-    "evanleck/vim-svelte",
-    ft = { "svelte" },
-  },
-  -- JavaScript plugin
-  {
-    "pangloss/vim-javascript",
-    ft = { "javascript" },
-  },
-  -- TypeScript plugin (Yats)
-  {
-    "HerringtonDarkholme/yats.vim",
-    ft = { "typescript" },
-  },
+	-- Svelte
+	{
+		"evanleck/vim-svelte",
+		ft = { "svelte" },
+	},
+	-- JavaScript plugin
+	{
+		"pangloss/vim-javascript",
+		ft = { "javascript" },
+	},
+	-- TypeScript plugin (Yats)
+	{
+		"HerringtonDarkholme/yats.vim",
+		ft = { "typescript" },
+	},
 
-  -- Config
+	-- Config
 
-  {
-    "nvim-treesitter/nvim-treesitter",
-    config = function()
-      require("adavidson.plugins.treesitter")
-    end,
-  },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		config = function()
+			require("adavidson.plugins.treesitter")
+		end,
+	},
 
-  {
-    "windwp/nvim-autopairs",
-    event = "InsertEnter",
-    opts = {},
-  },
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		opts = {},
+	},
 
-  {
-    "lukas-reineke/indent-blankline.nvim",
-    config = function()
-      require("adavidson.plugins.indent-blankline")
-    end,
-  },
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		config = function()
+			require("adavidson.plugins.indent-blankline")
+		end,
+	},
 
-{'akinsho/bufferline.nvim', version = "*", dependencies = 'nvim-tree/nvim-web-devicons'},
+	{ "akinsho/bufferline.nvim", version = "*", dependencies = "nvim-tree/nvim-web-devicons" },
 
-  {
-    "numToStr/Comment.nvim",
-    -- opts = {},
-    -- config = function() require('adavidson.plugins.comment') end
-  },
+	{
+		"numToStr/Comment.nvim",
+		-- opts = {},
+		-- config = function() require('adavidson.plugins.comment') end
+	},
 
-  -- Git
+	-- Git
 
-  -- {
-  --   "lewis6991/gitsigns.nvim",
-  --   -- opts = {},
-  --   -- config = function() require('adavidson.plugins.gitsigns') end
-  -- },
+	{
+	  "lewis6991/gitsigns.nvim",
+	  opts = {},
+	  config = function() require('adavidson.plugins.gitsigns') end
+	},
 
-  -- Lsp
+	{
+		"kdheepak/lazygit.nvim",
+		lazy = false,
+		cmd = {
+			"LazyGit",
+			"LazyGitConfig",
+			"LazyGitCurrentFile",
+			"LazyGitFilter",
+			"LazyGitFilterCurrentFile",
+		},
+		-- optional for floating window border decoration
+		dependencies = {
+			"nvim-telescope/telescope.nvim",
+			"nvim-lua/plenary.nvim",
+		},
+		config = function()
+			require("telescope").load_extension("lazygit")
+		end,
+	},
+	-- Lsp
 
-  {
-    "mason-org/mason.nvim",
-    opts = {}
-  },
+	{
+		"mason-org/mason.nvim",
+		opts = {},
+	},
 
-  {
-    "nvimtools/none-ls.nvim",
-    config = function()
-      require("adavidson.plugins.lsp.null-ls")
-    end,
-  },
+	{
+		"nvimtools/none-ls.nvim",
+		config = function()
+			require("adavidson.plugins.lsp.null-ls")
+		end,
+	},
 
-  {
-    "neovim/nvim-lspconfig",
-    -- opts = {},
-  },
+	{
+		"neovim/nvim-lspconfig",
+		-- opts = {},
+	},
 
-  {
-    "hrsh7th/nvim-cmp", -- needs setup
-    -- opts = {},
-    -- config = function() require('adavidson.plugins.cmp') end
-  },
+	{
+		"hrsh7th/nvim-cmp", -- needs setup
+		-- opts = {},
+		-- config = function() require('adavidson.plugins.cmp') end
+	},
 
-  {
-    "hrsh7th/cmp-buffer", -- needs setup
-    -- opts = {},
-    config = function()
-      require("adavidson.plugins.cmp")
-    end,
-  },
+	{
+		"hrsh7th/cmp-buffer", -- needs setup
+		-- opts = {},
+		config = function()
+			require("adavidson.plugins.cmp")
+		end,
+	},
 
-  {
-    "hrsh7th/cmp-path", -- needs setup
-    -- opts = {},
-  },
+	{
+		"hrsh7th/cmp-path", -- needs setup
+		-- opts = {},
+	},
 
-  {
-    "saadparwaiz1/cmp_luasnip", -- needs setup
-    -- opts = {},
-  },
+	{
+		"saadparwaiz1/cmp_luasnip", -- needs setup
+		-- opts = {},
+	},
 
-  {
-    "hrsh7th/cmp-nvim-lsp", -- needs setup
-    -- opts = {},
-  },
+	{
+		"hrsh7th/cmp-nvim-lsp", -- needs setup
+		-- opts = {},
+	},
 
-  {
-    "hrsh7th/cmp-nvim-lua", -- needs setup
-    -- opts = {},
-  },
+	{
+		"hrsh7th/cmp-nvim-lua", -- needs setup
+		-- opts = {},
+	},
 
-  {
-    "L3MON4D3/LuaSnip", -- needs setup
-    dependencies = { "rafamadriz/friendly-snippets" },
-    -- opts = {},
-  },
+	{
+		"L3MON4D3/LuaSnip", -- needs setup
+		dependencies = { "rafamadriz/friendly-snippets" },
+		-- opts = {},
+	},
 
-  -- Shortcuts
+	-- Shortcuts
 
-  {
-    "folke/which-key.nvim",
-    lazy = false,
-    priority = 1000,
-    -- opts = {},
-    config = function()
-      require("adavidson.plugins.whichkey")
-    end,
-  },
+	{
+		"folke/which-key.nvim",
+		lazy = false,
+		priority = 1000,
+		-- opts = {},
+		config = function()
+			require("adavidson.plugins.whichkey")
+		end,
+	},
 
-  -- Search
+	-- Search
 
-  {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.5",
-    dependencies = { "nvim-lua/plenary.nvim", lazy = false },
-  },
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.5",
+		dependencies = { "nvim-lua/plenary.nvim", lazy = false },
+	},
 
-  -- Themes
+	-- Themes
 
-  -- {
-  --   "navarasu/onedark.nvim",
-  --   lazy = false,
-  --   priority = 1000,
-  --   -- opts = {},
-  --   config = function()
-  --     require("adavidson.theme.colorscheme")
-  --   end,
-  -- },
+	-- {
+	--   "navarasu/onedark.nvim",
+	--   lazy = false,
+	--   priority = 1000,
+	--   -- opts = {},
+	--   config = function()
+	--     require("adavidson.theme.colorscheme")
+	--   end,
+	-- },
 
-  {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    -- opts = {},
-    config = function()
-      require("adavidson.theme.colorscheme")
-    end,
-  },
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		-- opts = {},
+		config = function()
+			require("adavidson.theme.colorscheme")
+		end,
+	},
 
-  -- {
-  --   "shaunsingh/nord.nvim",
-  --   lazy = false,
-  --   priority = 1000,
-  --   -- opts = {},
-  --   config = function() require('adavidson.theme.colorscheme') end
-  -- },
+	-- {
+	--   "shaunsingh/nord.nvim",
+	--   lazy = false,
+	--   priority = 1000,
+	--   -- opts = {},
+	--   config = function() require('adavidson.theme.colorscheme') end
+	-- },
 
-  -- UI / Navigation
+	-- UI / Navigation
 
-  {
-    "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-      require("adavidson.plugins.lualine")
-    end,
-  },
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("adavidson.plugins.lualine")
+		end,
+	},
 
-  {
-    "kyazdani42/nvim-tree.lua",
-    -- opts = {},
-    config = function()
-      require("adavidson.plugins.nvim-tree")
-    end,
-  },
+	{
+		"kyazdani42/nvim-tree.lua",
+		-- opts = {},
+		config = function()
+			require("adavidson.plugins.nvim-tree")
+		end,
+	},
 
-  {
-    "akinsho/toggleterm.nvim",
-    -- opts = {},
-    config = function()
-      require("adavidson.plugins.toggleterm")
-    end,
-  },
+	{
+		"akinsho/toggleterm.nvim",
+		-- opts = {},
+		config = function()
+			require("adavidson.plugins.toggleterm")
+		end,
+	},
 
-  {
-    "ahmedkhalf/project.nvim",
-    -- opts = {}
-  },
+	{
+		"ahmedkhalf/project.nvim",
+		-- opts = {}
+	},
 
-  -- {
-  --   "lewis6991/impatient.nvim",
-  --   -- opts = {}
-  -- },
+	-- {
+	--   "lewis6991/impatient.nvim",
+	--   -- opts = {}
+	-- },
 
-  {
-    "goolord/alpha-nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-      require("adavidson.plugins.alpha")
-    end,
-  },
+	{
+		"goolord/alpha-nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("adavidson.plugins.alpha")
+		end,
+	},
 }
 
 require("lazy").setup(plugins, options)
